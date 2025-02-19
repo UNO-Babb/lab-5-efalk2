65716f4f77f77b0d64e802d0f238e244f9b83c82
efalk2 <efalk@unomaha.edu>
2025-02-19 16:12:42 +0000

    finished lab

--- WordGame.py
+++ WordGame.py
@@ -4,20 +4,34 @@ import random
 
 def inWord(letter, word):
     """Returns boolean if letter is anywhere in the given word"""
-
-    return False
+    spot = word.find(letter)
+    if spot >= 0:
+     return True
+    else:
+     return False
 
 def inSpot(letter, word, spot):
     """Returns boolean response if letter is in the given spot in the word."""
-
-    return False
+    if word[spot] == letter:
+     return True
+    else:
+     return False
 
 def rateGuess(myGuess, word):
     """Rates your guess and returns a word with the following features.
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
     - * if the letter is not in the word at all"""
-
+    feedback = ""
+    for spot in range(5):
+        letter = myGuess[spot]
+        if inSpot(letter, word, spot):
+            feedback = feedback + letter.upper()
+        elif inWord(letter, word):
+            feedback = feedback + letter.lower()
+        else:
+            feedback = feedback + "*"
+    return feedback
 
 def main():
     #Pick a random word from the list of all words
@@ -28,9 +42,21 @@ def main():
     print(todayWord)
 
     #User should get 6 guesses to guess
-
-    #Ask user for their guess
-    #Give feedback using on their word:
+    guessNum = 1
+    feedback = ""
+    while guessNum < 6 and feedback != todayWord.upper():
+        guess = "aaaaa"
+        while guess not in wordList:
+            guess = input("Enter a word: ")
+        feedback = rateGuess(guess, todayWord)
+        print(feedback)
+        guessNum = guessNum + 1
+        #Ask user for their guess
+        #Give feedback using on their word:
+    if feedback == todayWord.upper():
+      print("Congratulations! You got it!")
+    else:
+      print("Wrong! Try again!")
 
 
 
